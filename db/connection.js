332c7df7059947615c3dd22011e3ca4982138d20
Mohammad Rahman <mbrahman@gmail.com>
2022-03-15 11:50:34 -0400

    promisified db connection

--- db/connection.js
+++ db/connection.js
@@ -1,18 +1,34 @@
-const db = require('mysql2')
+const mysql = require('mysql2')
 const fs = require('fs')
 
-db.createConnection(
-    {
-        host: 'db.stippled.art',
-        ssl: {
-            ca: fs.readFileSync('././certs/server-ca.cer'),
-            cert: fs.readFileSync('././certs/client-cert.cer'),
-            key: fs.readFileSync('././certs/client-key.cer')
-        },
-        user: 'root',
-        password: 'Cisco123',
-        database: 'employees'
+// const connection = await mysql.createConnection(
+//     {
+//         host: 'db.stippled.art',
+//         ssl: {
+//             ca: fs.readFileSync('././certs/server-ca.cer'),
+//             cert: fs.readFileSync('././certs/client-cert.cer'),
+//             key: fs.readFileSync('././certs/client-key.cer')
+//         },
+//         user: 'root',
+//         password: 'Cisco123',
+//         database: 'employees'
+//     },
+//     console.log('Connected to employees database.')
+// )
+
+// module.exports = connection
+
+const db = mysql.createConnection({
+    host: 'db.stippled.art',
+    ssl: {
+        ca: fs.readFileSync('././certs/server-ca.cer'),
+        cert: fs.readFileSync('././certs/client-cert.cer'),
+        key: fs.readFileSync('././certs/client-key.cer')
     },
+    user: 'root',
+    password: 'Cisco123',
+    database: 'employees'
+},
     console.log('Connected to employees database.')
 )
 
--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 const db = require('./db/connection')
+
+db.promise().query(`SELECT * FROM employee`)
+    .then(([rows]) => {
+        console.log(rows)
+    })
