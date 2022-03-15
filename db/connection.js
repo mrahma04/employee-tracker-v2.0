19b0fc38d983bc6088be8d2de6ee2942db1dcf8f
Mohammad Rahman <mbrahman@gmail.com>
2022-03-15 09:03:45 -0400

    successfully connected to employees database

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+const db = require('mysql2')
+const fs = require('fs')
+
+db.createConnection(
+    {
+        host: 'db.stippled.art',
+        ssl: {
+            ca: fs.readFileSync('././certs/server-ca.cer'),
+            cert: fs.readFileSync('././certs/client-cert.cer'),
+            key: fs.readFileSync('././certs/client-key.cer')
+        },
+        user: 'root',
+        password: 'Cisco123',
+        database: 'employees'
+    },
+    console.log('Connected to employees database.')
+)
+
+module.exports = db
